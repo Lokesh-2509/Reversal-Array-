cd0ed57a0940356d0c0dc37de6aa81975f94f57b
lokeshbalu25 <lokeshbalu25@gmail.com>
2023-07-16 19:31:11 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def reverse_array(arr):
+    return arr[::-1]
+A = [1, 2, 3, 2, 1]
+reversed_array = reverse_array(A)
+print("Reversed array:", reversed_array)
+A = [1, 1, 10]
+reversed_array = reverse_array(A)
+print("Reversed array:", reversed_array)
